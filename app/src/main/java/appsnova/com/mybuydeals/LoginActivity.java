f0341e1019231d1088aefb1bd0a33ad740d619c1
Maddy <venkat.madhu01@gmail.com>
2019-01-09 17:27:05 -0800

    Login button code added

--- app/src/main/java/appsnova/com/mybuydeals/LoginActivity.java
+++ app/src/main/java/appsnova/com/mybuydeals/LoginActivity.java
@@ -1,9 +1,12 @@
  package appsnova.com.mybuydeals;
 
+import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
