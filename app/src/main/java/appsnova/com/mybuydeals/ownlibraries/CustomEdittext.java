db87f8748cdc94c4b148df3158f8da9ba3516485
Venkata Madhu <venkat.madhu03@gmail.com>
2019-01-29 18:59:18 +0530

    Created new Files

---
+++ app/src/main/java/appsnova/com/mybuydeals/ownlibraries/CustomEdittext.java
@@ -0,0 +1,23 @@
+package appsnova.com.mybuydeals.ownlibraries;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import appsnova.com.mybuydeals.R;
+
+
+public class CustomEdittext extends TextInputEditText {
+    public CustomEdittext(Context context) {
+        super(context);
+    }
+
+    public CustomEdittext(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomEdittext(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
